52f22ec578439b6ba2eaca8e992a1f88fcf8c52d
dpull <beingstudio@gmail.com>
2022-07-27 00:13:56 +0800

    test webdav code.

--- cmd/coffer/main.go
+++ cmd/coffer/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "log"
-
+import (
+	"flag"
+	"golang.org/x/net/webdav"
+	"net/http"
+)
+/*
+http.ListenAndServe(":8080", &webdav.Handler{
+        FileSystem: webdav.Dir("."),
+        LockSystem: webdav.NewMemLS(),
+    })
+*/
 func main() {
-	log.Print("hello")
+	flag.Parse()
+	fs := &webdav.Handler{
+		FileSystem: webdav.Dir(""),
+		LockSystem: webdav.NewMemLS(),
+	}
+	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		// username, password, ok := req.BasicAuth()
+
+		if handleDirList(fs.FileSystem, w, req) {
+			return
+		}
+		fs.ServeHTTP(w, req)
+	})
+	http.ListenAndServe(":8080", nil)
+
+}
+func handleDirList(fs webdav.FileSystem, w http.ResponseWriter, req *http.Request) bool {
+	return false
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/dpull/coffer
 
 go 1.18
+
+require golang.org/x/net v0.0.0-20220725212005-46097bf591d3
