d4c3ad8549e5dd51555dfdfb6a6f15e5baf14b3e
dpull <beingstudio@gmail.com>
2022-07-27 09:55:13 +0800

    project init.

--- cmd/coffer/main.go
+++ cmd/coffer/main.go
@@ -1,33 +1,49 @@
 package main
 
 import (
-	"flag"
-	"golang.org/x/net/webdav"
+	"io"
+	"log"
 	"net/http"
+	"os"
+
+	"github.com/dpull/coffer/filesystem"
+	"golang.org/x/net/webdav"
 )
-/*
-http.ListenAndServe(":8080", &webdav.Handler{
-        FileSystem: webdav.Dir("."),
-        LockSystem: webdav.NewMemLS(),
-    })
-*/
-func main() {
-	flag.Parse()
-	fs := &webdav.Handler{
-		FileSystem: webdav.Dir(""),
-		LockSystem: webdav.NewMemLS(),
-	}
-	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		// username, password, ok := req.BasicAuth()
 
-		if handleDirList(fs.FileSystem, w, req) {
-			return
-		}
-		fs.ServeHTTP(w, req)
-	})
-	http.ListenAndServe(":8080", nil)
-
+type config struct {
+	HttpAddr string
+	Folder   string
+	FSType   string
+	FSParam  map[string]string
 }
-func handleDirList(fs webdav.FileSystem, w http.ResponseWriter, req *http.Request) bool {
-	return false
+
+func main() {
+	log.SetFlags(log.Lshortfile | log.Ldate | log.Lmicroseconds)
+
+	fd, err := os.OpenFile("log.txt", os.O_WRONLY|os.O_CREATE, 0755)
+	if err != nil {
+		log.Fatalf("create file log.txt failed: %+v", err)
+	}
+	defer fd.Close()
+	log.SetOutput(io.MultiWriter(fd, os.Stdout))
+
+	// TODO read from config
+	config := config{
+		HttpAddr: "localhost:8080",
+		Folder:   "./temp",
+		FSType:   "xor",
+	}
+
+	fs, err := filesystem.Create(config.FSType, config.Folder, config.FSParam)
+	if err != nil {
+		log.Fatalf("create file system failed: %+v", err)
+	}
+
+	err = http.ListenAndServe(config.HttpAddr, &webdav.Handler{
+		FileSystem: fs,
+		LockSystem: webdav.NewMemLS(),
+	})
+	if err != nil {
+		log.Fatalf("http serve failed: %+v", err)
+	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/dpull/coffer
 
 go 1.18
 
-require golang.org/x/net v0.0.0-20220725212005-46097bf591d3
+require (
+	github.com/pkg/errors v0.9.1
+	golang.org/x/net v0.0.0-20220725212005-46097bf591d3
+)
