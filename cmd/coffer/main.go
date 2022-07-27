8aac23a3f58aaca7f6abb05c46be978921abf10b
dpull <beingstudio@gmail.com>
2022-07-27 13:31:30 +0800

    config support.

--- cmd/coffer/main.go
+++ cmd/coffer/main.go
@@ -1,45 +1,65 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
+	"github.com/dpull/coffer/filesystem"
+	"golang.org/x/net/webdav"
 	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
-
-	"github.com/dpull/coffer/filesystem"
-	"golang.org/x/net/webdav"
 )
 
 type config struct {
-	HttpAddr string
-	Folder   string
-	FSType   string
-	FSParam  map[string]string
+	HttpAddr string            `json:"http_addr"`
+	Folder   string            `json:"folder"`
+	FSType   string            `json:"fs_type"`
+	FSParam  map[string]string `json:"fs_param"`
+}
+
+func initLog() (*os.File, error) {
+	log.SetFlags(log.Lshortfile | log.Ldate | log.Lmicroseconds)
+	fd, err := os.OpenFile("log.txt", os.O_WRONLY|os.O_CREATE, 0755)
+	if err != nil {
+		return nil, err
+	}
+	log.SetOutput(io.MultiWriter(fd, os.Stdout))
+	return fd, nil
+}
+
+func readConfig(conf *config) error {
+	path := flag.String("c", "config.json", "config file")
+	flag.Parse()
+
+	data, err := ioutil.ReadFile(*path)
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(data, conf)
 }
 
 func main() {
-	log.SetFlags(log.Lshortfile | log.Ldate | log.Lmicroseconds)
-
-	fd, err := os.OpenFile("log.txt", os.O_WRONLY|os.O_CREATE, 0755)
+	fd, err := initLog()
 	if err != nil {
-		log.Fatalf("create file log.txt failed: %+v", err)
+		log.Fatalf("init log failed:%+v", err)
 	}
 	defer fd.Close()
-	log.SetOutput(io.MultiWriter(fd, os.Stdout))
 
-	// TODO read from config
-	config := config{
-		HttpAddr: "localhost:8080",
-		Folder:   "./temp",
-		FSType:   "xor",
+	var conf config
+	err = readConfig(&conf)
+	if err != nil {
+		flag.Usage()
+		log.Fatalf("read config failed:%=v", err)
 	}
 
-	fs, err := filesystem.Create(config.FSType, config.Folder, config.FSParam)
+	fs, err := filesystem.Create(conf.FSType, conf.Folder, conf.FSParam)
 	if err != nil {
 		log.Fatalf("create file system failed: %+v", err)
 	}
 
-	err = http.ListenAndServe(config.HttpAddr, &webdav.Handler{
+	err = http.ListenAndServe(conf.HttpAddr, &webdav.Handler{
 		FileSystem: fs,
 		LockSystem: webdav.NewMemLS(),
 	})
